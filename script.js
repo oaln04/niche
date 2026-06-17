660d003168729a2cc443a2ee8e0383b38e02c2f3
Omar Al Nuaimi <it.trainee1@rakez.com>
2026-06-17 13:12:10 +0400

    question bank & answers

---
+++ script.js
@@ -0,0 +1,58 @@
+// larper, egirl, gymbro, khaleeji, looksmaxxer, ecomm 
+
+const questions = [
+    {
+        question: "it's 2am. you're not tired. you —",
+        answers: [
+            { text: "try to read a book and give up after 2 pages.", scores: { larper: 3, gymbro: 1 } },
+            { text: "get on val to find huzz.", scores: { egirl: 3, larper: 1, khaleeji: 1 } },
+            { text: "pinterest. no reason.", scores: { khaleeji: 3, larper: 1, egirl: 1 } },
+            { text: "check shopify.", scores: { ecomm: 3, looksmaxxer: 1 } }
+        ]
+    },
+    {
+        question: "you're about to go to university, you —",
+        answers: [
+            { text: "buy a matcha before class obviously.", scores: { khaleeji: 3, larper: 2 } },
+            { text: "make sure your shirt is tight on your arms.", scores: { gymbro: 3, looksmaxxer: 1 } },
+            { text: "take your laptop and nothing else.", scores: { ecomm: 3, larper: 1 } },
+            { text: "check your night market and get on disc.", scores: { egirl: 3, larper: 1 } }
+        ]
+    },
+    {
+        question: "you have a date, you —",
+        answers: [
+            { text: "wear a ribbed tank and a chain", scores: { gymbro: 3, looksmaxxer: 1 } },
+            { text: "meet up in the car, duh.", scores: { khaleeji: 3} },
+            { text: "put on your catears and join vc", scores: { egirl: 3, larper: 1} },
+            { text: "have no time for dates", scores: { ecomm: 3, looksmaxxer: 2} }
+        ]
+    },
+    {
+        question: "your dream destination is —",
+        answers: [
+            { text: "lndn.", scores: { khaleeji: 3, larper: 1 } },
+            { text: "tokyo.", scores: { egirl: 3, larper: 1 } },
+            { text: "dubai.", scores: { ecomm: 3, khaleeji: 1 } },
+            { text: "india.", scores: { looksmaxxer: 3, ecomm: 1 } }
+        ]
+    },
+    {
+        question: "your favorite app is —",
+        answers: [
+            { text: "tiktok.", scores: { looksmaxxer: 3} },
+            { text: "discord.", scores: { egirl: 3}},
+            { text: "twitter.", scores: { khaleeji: 3}},
+            { text: "letterboxd.", scores: { larper: 3}}
+        ]
+    },
+    {
+        question: "you're at a restaurant, you —",
+        answers: [
+            { text: "don't eat.", scores: { looksmaxxer: 3, egirl: 2, khaleeji: 1 } },
+            { text: "order the cheapest thing and take pics.", scores: { larper: 3, ecomm: 1 } },
+            { text: "get a protein shake to go.", scores: { gymbro: 3, looksmaxxer: 1 } },
+            { text: "get the most expensive thing on the menu.", scores: { ecomm: 3, khaleeji: 1 } }
+        ]
+    }
+];
